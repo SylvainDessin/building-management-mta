a5077f32c70a83c7ea498916443bf9672bbc16b7
Sopra Inno <soprainnovationsap@outlook.com>
2023-06-29 14:55:57 +0000

    Project template

---
+++ buildingmanagement/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["fioriChickenFarm/buildingmanagement/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ buildingmanagement/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"fioriChickenFarm/buildingmanagement/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
